5b84e9ce0b6a01193b01af5be4cd5051a1560df1
Lucas Dachman <lucas.dachman@gmail.com>
2018-12-05 21:46:09 -0700

    add nav overlay

--- portfolio/js/sidebar.js
+++ portfolio/js/sidebar.js
@@ -2,10 +2,13 @@
 const sidebar = document.getElementsByClassName('sidebar')[0];
 const navItems = document.getElementsByClassName('nav-item');
 const dots = document.getElementsByClassName('dot');
+const overlay = document.getElementsByClassName('overlay')[0];
 
 
 sidebar.onmouseleave = () => {
+  overlay.style.backgroundColor = 'transparent';
   setTimeout(() => {
+    overlay.style.visibility = 'hidden';
     [].forEach.call(dots, (dot) => {
       dot.style.display = 'block';
     });
@@ -16,6 +19,8 @@ sidebar.onmouseleave = () => {
 };
 
 sidebar.onmouseenter = () => {
+  overlay.style.visibility = 'visible';
+  overlay.style.backgroundColor = 'rgba(100%, 100%, 100%, 0.95)';
   [].forEach.call(dots, (dot) => {
     dot.style.display = 'none';
   });
