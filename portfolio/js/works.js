67e005632c95daa8a20013c05ab60c3fb7a0c408
Lucas Dachman <lucas.dachman@gmail.com>
2018-12-05 20:19:07 -0700

    add sidebar to works page

--- portfolio/js/works.js
+++ portfolio/js/works.js
@@ -31,8 +31,33 @@ const populateCards = () => {
 
   const worksGrid = document.getElementById('works-grid');
   cards.forEach((cardElement) => {
-    console.log(cardElement);
     worksGrid.innerHTML += cardElement;
+  });
+
+}
+
+const sidebar = document.getElementById('sidebar');
+const navItems = document.getElementsByClassName('nav-item');
+const dots = document.getElementsByClassName('dot');
+
+
+sidebar.onmouseleave = () => {
+  setTimeout(() => {
+    [].forEach.call(dots, (dot) => {
+      dot.style.display = 'block';
+    });
+    [].forEach.call(navItems, (item) => {
+      item.style.display = 'none';
+    });
+  }, 300);
+};
+
+sidebar.onmouseenter = () => {
+  [].forEach.call(dots, (dot) => {
+    dot.style.display = 'none';
+  });
+  [].forEach.call(navItems, (item) => {
+    item.style.display = 'block';
   });
 
 }
