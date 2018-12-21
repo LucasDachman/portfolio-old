fa46887cf4631d944e455ac4ca59dda1240b0880
Lucas Dachman <lucas.dachman@gmail.com>
2018-12-21 14:58:47 -0600

    change card styles and add more works

--- js/works.js
+++ js/works.js
@@ -1,6 +1,6 @@
-const cardTemplate = (title, description, image, url="") => {
+const cardTemplate = (description, image, url="") => {
   const card = /*html*/`
-    <a href='${url}' class='work-link'>
+    <a href='${url}' target='_blank' class='work-link'>
       <img src='${image}' class='work-image'>
       <p class='work-description'>${description}</p>
     </a>
@@ -10,24 +10,24 @@ const cardTemplate = (title, description, image, url="") => {
 
 const populateCards = () => {
   const cards = [
-    cardTemplate('Wearable Midi Controller', `A wearable device that
+    cardTemplate(`A wearable device that
     detects distance and sends the data to any music software.`,
     '../img/wearable-midi.JPG', 
     'https://lucasdachman.github.io/blog/atlas-object/project-2'),
-    cardTemplate('Origami Circuit', `Simple origami
+    cardTemplate(`An iOS app that does common circuit calculations`,
+    './img/cirkit-logo.png',
+    'https://lucasdachman.github.io/blog/mobile-apps/milestone-3'),
+    cardTemplate(`An app that calculates
+    resistor values according to color codes.`,'../img/resistor-calc.png', 
+    'https://lucasdachman.github.io/blog/mobile-apps/p2-milestone-4'),
+    cardTemplate(`An exercise to work on CSS techniques.`,
+    '../img/css-zen.png', 
+    'https://creative.colorado.edu/~luda3983/web/projects/zen-garden/index.html'),
+    cardTemplate(`A moving typographic sculpture.`, 
+    './img/wakemake.png', 'https://lucasdachman.github.io/blog/typography/wake-make'),
+    cardTemplate(`Simple origami
     with LEDs.`, './img/Origami-Circuit.gif', 
     'https://lucasdachman.github.io/blog/atlas-object/Origami-Circuit'),
-    cardTemplate('Cirkit', `An iOS app that calculates
-    resistor values according to color codes.`,'./img/cirkit-logo.png', 
-    'https://lucasdachman.github.io/blog/mobile-apps/milestone-4'
-    ),
-    cardTemplate('Kinetic Typography', `A moving 
-    typographic sculpture.`, 
-    './img/wakemake.png', 'https://lucasdachman.github.io/blog/typography/wake-make'),
-    cardTemplate('Title 4', `A project about 
-    things where I used this and that to do 
-    it a way that is better because its really
-    cool.`, 'https://via.placeholder.com/400x400'),
   ];
 
   const worksGrid = document.getElementById('works-grid');
