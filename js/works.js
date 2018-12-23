238f821271b297cc3251cdc9b76f1e29de5a9382
Lucas Dachman <lucas.dachman@gmail.com>
2018-12-22 20:26:52 -0600

    correct img paths

--- js/works.js
+++ js/works.js
@@ -12,16 +12,16 @@ const populateCards = () => {
   const cards = [
     cardTemplate(`A wearable device that
     detects distance and sends the data to any music software.`,
-    '../img/wearable-midi.JPG', 
+    './img/wearable-midi.JPG', 
     'https://lucasdachman.github.io/blog/atlas-object/project-2'),
     cardTemplate(`An iOS app that does common circuit calculations`,
     './img/cirkit-logo.png',
     'https://lucasdachman.github.io/blog/mobile-apps/milestone-3'),
     cardTemplate(`An app that calculates
-    resistor values according to color codes.`,'../img/resistor-calc.png', 
+    resistor values according to color codes.`,'./img/resistor-calc.png', 
     'https://lucasdachman.github.io/blog/mobile-apps/p2-milestone-4'),
     cardTemplate(`An exercise to work on CSS techniques.`,
-    '../img/css-zen.png', 
+    './img/css-zen.png', 
     'https://creative.colorado.edu/~luda3983/web/projects/zen-garden/index.html'),
     cardTemplate(`A moving typographic sculpture.`, 
     './img/wakemake.png', 'https://lucasdachman.github.io/blog/typography/wake-make'),
